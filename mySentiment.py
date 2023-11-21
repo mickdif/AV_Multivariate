d93cc0a41b8a55ab18dc3c75c2f67dd2791c27d2
Michele di Fede <118694007+mickdif@users.noreply.github.com>
2023-11-21 12:07:11 +0100

    Update mySentiment.py

--- mySentiment.py
+++ mySentiment.py
@@ -2,58 +2,45 @@ from gnews import GNews # scrape
 from deep_translator import GoogleTranslator # translate
 from textblob import TextBlob # evaluate
 
+fmt = "%d/%m/%Y"
+
+
 class Sentiment_Analysis:
-    def __init__(self, query, language, country, start, end):
-        self.google_news = GNews(language=language, country=country, start_date=start, end_date=end, max_results=1000)         
+    def __init__(self, query, start, end):   
 
-        # altri esempi che danno problemi
-        # self.google_news = GNews(language=language, country=country, start_date=(2019, 1, 1), end_date=(2020, 1, 1), max_results=1000) 
-        # self.google_news = GNews(language=language, country=country, period="5y") 
+        self.google_news = GNews(language='it', country='it', start_date=start, end_date=end, max_results=3)
         self.json_resp = self.google_news.get_news(query)
-        self.gt = GoogleTranslator(source=language, target='en')
-        self.idx = 0
-
-    def do_Analysis(self, date, debug=False): # per singolo giorno
-        polarity_tot = 0.0
-
-        while self.idx < len(self.json_resp):
-            article = self.google_news.get_full_article(self.json_resp[self.idx]['url']) # newspaper3k instance
-            
-            if article.publish_date  != date:
-                if debug == True: print(article.publish_date, " != ", date)
-                break
-
-            self.idx += 1
-            
+        self.polarity_tot = 0.0
+        self.gt = GoogleTranslator(source='it', target='en')
+    def do_Analysis(self, debug=False):
+        aperti = 0
+        for idx in range(len(self.json_resp)):
+            article = self.google_news.get_full_article(self.json_resp[idx]['url'])            
             try:
-                # commentato per mettere in risalto altri problemi
-                # if self.query not in article.title: 
-                # if debug == True: print(self.query, " non in ", article.title)
-                    # continue
-
+                if debug == True: print(article.title)
                 # open and translate
                 text = article.text
                 text = self.gt.translate(text=text)
+                if debug == True: print("art tradotto")
                 
-                if debug == True:
-                    print("debug")
-                    print(article.title)
-                    print(article.publish_date)
                 
             except:
                 try:
-                    text = article.description
+                    text = article.title
                     text = self.gt.translate(text=text)
+                    if debug == True: print("titolo tradotto")
                 except :
-                    if debug == True: print("errore nell'apertura dell'articolo")
+                    if debug == True: print("errore nell'apertura o traduzione dell'articolo")
                     continue # ignore exceptions
                 
             try:
                 analysis = TextBlob(text)
-                polarity_tot += analysis.sentiment.polarity
-                if debug == True: print(analysis.sentiment) 
+                self.polarity_tot += analysis.sentiment.polarity
+                if debug == True: print(self.polarity_tot) 
             except:
-                if debug == True: print("errore nella traduzione")
+                if debug == True: print("errore nell'analisi")
                 continue # ignore exceptions
-           
-        return polarity_tot     
+            aperti += 1.0
+         
+        if aperti != 0: return self.polarity_tot/aperti
+        else: return 0.0
