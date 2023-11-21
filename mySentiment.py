54f6e7c7737d9c91332ad4cf7bc5af83da89c24b
Michele di Fede <118694007+mickdif@users.noreply.github.com>
2023-11-21 16:06:09 +0100

    Update mySentiment.py

--- mySentiment.py
+++ mySentiment.py
@@ -6,41 +6,39 @@ fmt = "%d/%m/%Y"
 
 
 class Sentiment_Analysis:
-    def __init__(self, query, start, end):   
-
-        self.google_news = GNews(language='it', country='it', start_date=start, end_date=end, max_results=3)
-        self.json_resp = self.google_news.get_news(query)
-        self.polarity_tot = 0.0
+    def __init__(self):   
         self.gt = GoogleTranslator(source='it', target='en')
-    def do_Analysis(self, debug=False):
+        
+    def do_Analysis(self, query, start, end, debug=False):
+        google_news = GNews(language='it', country='it', start_date=start, end_date=end, max_results=5)
+        json_resp = google_news.get_news(query)
+        polarity_tot = 0.0
         aperti = 0
-        for idx in range(len(self.json_resp)):
-            article = self.google_news.get_full_article(self.json_resp[idx]['url'])            
+        for idx in range(len(json_resp)):
+            article = google_news.get_full_article(json_resp[idx]['url'])            
             try:
                 if debug == True: print(article.title)
                 # open and translate
                 text = article.text
-                text = self.gt.translate(text=text)
-                if debug == True: print("art tradotto")
-                
+                text = self.gt.translate(text=text)                
                 
             except:
-                try:
+                try: # se non riesce col testo prova col solo titolo
                     text = article.title
                     text = self.gt.translate(text=text)
-                    if debug == True: print("titolo tradotto")
                 except :
                     if debug == True: print("errore nell'apertura o traduzione dell'articolo")
                     continue # ignore exceptions
                 
             try:
                 analysis = TextBlob(text)
-                self.polarity_tot += analysis.sentiment.polarity
-                if debug == True: print(self.polarity_tot) 
+                polarity_tot += analysis.sentiment.polarity
+                if debug == True: print(polarity_tot) 
             except:
                 if debug == True: print("errore nell'analisi")
                 continue # ignore exceptions
-            aperti += 1.0
-         
-        if aperti != 0: return self.polarity_tot/aperti
+            if analysis.sentiment.polarity != 0: aperti += 1.0
+            
+        if debug == True: print("Analizzati correttamente: ", aperti, " su ", len(json_resp))         
+        if aperti != 0: return polarity_tot/aperti
         else: return 0.0
